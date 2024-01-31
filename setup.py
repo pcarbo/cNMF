b76ddd0f5df98a835732629187be493afcb1a7dc
Peter Carbonetto <peter.carbonetto@gmail.com>
2024-01-31 14:35:15 -0600

    Fixed normalize_total call.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="cnmf",
-    version="1.4.2",
+    version="1.4.3",
     author="Dylan Kotliar",
     author_email="dylkot@gmail.com",
     description="Consensus NMF for scRNA-Seq data",
